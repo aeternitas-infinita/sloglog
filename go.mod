c36d7e11baec4e808adf47279b103f0a3dda0c2a
Nikita Mosin <aeternitas.infinita@icloud.com>
2025-06-19 13:06:29 +0700

    Initial commit

---
+++ go.mod
@@ -0,0 +1,14 @@
+go 1.24.4
+
+module github.com/aeternitas-infinita/sloglog
+
+require (
+	github.com/joho/godotenv v1.5.1
+	github.com/valyala/fasthttp v1.62.0
+)
+
+require (
+	github.com/andybalholm/brotli v1.1.1 // indirect
+	github.com/klauspost/compress v1.18.0 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+)
---
+++ service.go
@@ -0,0 +1,102 @@
+package sloglog
+
+import (
+	"context"
+	"fmt"
+	"log/slog"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/valyala/fasthttp"
+)
+
+var Logger *slog.Logger
+var LoggerMin *slog.Logger
+
+func GetTraceID(ctx any) string {
+	if ctx == nil {
+		return ""
+	}
+	if requestCtx, ok := ctx.(*fasthttp.RequestCtx); ok {
+		if v := requestCtx.UserValue("trace_id"); v != nil {
+			return v.(string)
+		}
+	}
+	return ""
+}
+
+func LogWithContext(ctx context.Context, level slog.Level, msg string, args ...any) {
+	traceID := GetTraceID(ctx)
+	if traceID != "" {
+		newArgs := make([]any, 0, len(args)+1)
+		newArgs = append(newArgs, slog.String("trace_id", traceID))
+		newArgs = append(newArgs, args...)
+		args = newArgs
+	}
+	Logger.Log(ctx, level, msg, args...)
+}
+
+func InfoContext(ctx context.Context, msg string, args ...any) {
+	LogWithContext(ctx, slog.LevelInfo, msg, args...)
+}
+
+func DebugContext(ctx context.Context, msg string, args ...any) {
+	LogWithContext(ctx, slog.LevelDebug, msg, args...)
+}
+
+func WarnContext(ctx context.Context, msg string, args ...any) {
+	LogWithContext(ctx, slog.LevelWarn, msg, args...)
+}
+
+func ErrorContext(ctx context.Context, msg string, args ...any) {
+	LogWithContext(ctx, slog.LevelError, msg, args...)
+}
+
+func init() {
+	var level slog.Level
+	envLoggerLvl := os.Getenv("LOGGER_LEVEL")
+	if envLoggerLvl == "" {
+		err := godotenv.Load()
+		if err != nil {
+			slog.Error("error loading dotenv and logger level is not set", slog.Any("info", err))
+			os.Exit(1)
+		} else {
+			envLoggerLvl = os.Getenv("LOGGER_LEVEL")
+		}
+	}
+	switch envLoggerLvl {
+	case "debug":
+		level = slog.LevelDebug
+	case "warn":
+		level = slog.LevelWarn
+	case "error":
+		level = slog.LevelError
+	default:
+		level = slog.LevelInfo
+	}
+
+	replace := func(groups []string, a slog.Attr) slog.Attr {
+		if a.Key == slog.SourceKey {
+			source := a.Value.Any().(*slog.Source)
+			source.File = fmt.Sprintf("[%s:%d]", source.File, source.Line)
+		}
+		return a
+	}
+
+	opts := &slog.HandlerOptions{
+		AddSource:   true,
+		Level:       level,
+		ReplaceAttr: replace,
+	}
+	handler := slog.NewTextHandler(os.Stdout, opts)
+	optsMin := &slog.HandlerOptions{
+		AddSource: false,
+		Level:     level,
+	}
+	handlerMin := slog.NewTextHandler(os.Stdout, optsMin)
+
+	Logger = slog.New(handler)
+	LoggerMin = slog.New(handlerMin)
+
+	return
+}
